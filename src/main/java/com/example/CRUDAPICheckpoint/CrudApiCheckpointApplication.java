2bc72f0573d786f0e68f7a6232fde982d802a330
shortf15e <73486744+shortf15e@users.noreply.github.com>
2020-12-11 14:45:18 -0800

    Checked dev code in postman with non-test db

--- src/main/java/com/example/CRUDAPICheckpoint/CrudApiCheckpointApplication.java
+++ src/main/java/com/example/CRUDAPICheckpoint/CrudApiCheckpointApplication.java
@@ -3,13 +3,16 @@ package com.example.CRUDAPICheckpoint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.persistence.criteria.CriteriaBuilder;
+
 
 @SpringBootApplication
 public class CrudApiCheckpointApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(CrudApiCheckpointApplication.class, args);
-
+		Initialize init  = new Initialize();
+		init.setupDataBase();
 
 	}
 
--- src/main/java/com/example/CRUDAPICheckpoint/Initialize.java
+++ src/main/java/com/example/CRUDAPICheckpoint/Initialize.java
@@ -1,20 +1,31 @@
 package com.example.CRUDAPICheckpoint;
 
+
+import java.io.FileReader;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Initialize {
-
-    // intialize the database
-    String json = getJSON("/init.json");
 
     public Initialize() throws Exception {
     }
 
-    public String getJSON(String path) throws Exception{
+    public void setupDataBase () throws Exception {
+        String json = getJSON("/init.json");
+        System.out.println(json);
+        List<User> users = new ArrayList<User>();
+
+
+
+    }
+
+    private String getJSON(String path) throws Exception{
         URL url = this.getClass().getResource(path);
         return new String(Files.readAllBytes(Paths.get(url.getFile())));
     }
+
 
 }
