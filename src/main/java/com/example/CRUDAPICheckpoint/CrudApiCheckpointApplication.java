7410d14b92f8f6b8e82fe823c705bfcaeb179eac
shortf15e <73486744+shortf15e@users.noreply.github.com>
2020-12-11 09:18:57 -0800

    Initial commit

---
+++ src/main/java/com/example/CRUDAPICheckpoint/CrudApiCheckpointApplication.java
@@ -0,0 +1,13 @@
+package com.example.CRUDAPICheckpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApiCheckpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApiCheckpointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CRUDAPICheckpoint/CrudApiCheckpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRUDAPICheckpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiCheckpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
